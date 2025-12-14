c3f6a392690a75af6359cfac3c505e32592c192a
QBit Tech <qbittech.team@gmail.com>
2025-12-14 19:51:21 +0600

    .

--- app/Http/Controllers/Admin/MembersController.php
+++ app/Http/Controllers/Admin/MembersController.php
@@ -37,6 +37,7 @@ class MembersController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    
     public function store(Request $request)
     {
         $validatedData = $request->validate([
